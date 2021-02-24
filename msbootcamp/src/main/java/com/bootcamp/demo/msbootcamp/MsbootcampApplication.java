7da79b5d02f6c79f4e6ef53e25f1607f5a85a983
unknown <w2cluster738@iiht.tech>
2021-02-24 12:27:56 +0530

    First lab handson

---
+++ msbootcamp/src/main/java/com/bootcamp/demo/msbootcamp/MsbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.demo.msbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbootcampApplication.class, args);
+	}
+
+}
